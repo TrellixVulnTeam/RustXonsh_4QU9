e6b641b634683aaf48e60a902fb2c89a4c076b7e
Peter Ye <yaxollum@gmail.com>
2021-08-09 19:40:54 -0400

    Implement subproc uncaptured

--- vm/src/stdlib/rustxonsh.rs
+++ vm/src/stdlib/rustxonsh.rs
@@ -2,13 +2,25 @@ pub(crate) use decl::make_module;
 
 #[pymodule(name = "rustxonsh")]
 mod decl {
-    use crate::{builtins::PyStrRef, pyobject::PySequence, vm::VirtualMachine};
+    use crate::{
+        builtins::PyStrRef,
+        pyobject::{PyResult, PySequence},
+        vm::VirtualMachine,
+    };
+    use std::process::Command;
 
     #[pyfunction]
-    fn subproc_uncaptured(cmd: PyStrRef, args: PySequence<PyStrRef>, _vm: &VirtualMachine) -> () {
-        println!("cmd: {}", cmd.as_str());
-        for (arg, i) in args.into_vec().iter().zip(1..) {
-            println!("arg {}: {}", i, arg.as_str());
+    fn subproc_uncaptured(
+        cmd: PyStrRef,
+        args: PySequence<PyStrRef>,
+        vm: &VirtualMachine,
+    ) -> PyResult {
+        match Command::new(cmd.as_str())
+            .args(args.into_vec().iter().map(|s| s.as_str()))
+            .status()
+        {
+            Ok(_) => Ok(vm.ctx.none()),
+            Err(e) => Err(vm.new_os_error(e.to_string())),
         }
     }
 }
